b3e3650577a1c151b491962bd0667bfee89a437b
Hugo Machado Mendes da Silva <hugo.mendes@estudante.ifgoiano.edu.br>
2025-10-31 05:28:28 -0300

    :recycle: refactor: Refatora Mensagem

--- backend/src/main/java/br/com/hugomachadodev/BackendGerenciaDeConfiguracao/BackendGerenciaDeConfiguracaoApplication.java
+++ backend/src/main/java/br/com/hugomachadodev/BackendGerenciaDeConfiguracao/BackendGerenciaDeConfiguracaoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendGerenciaDeConfiguracaoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("==> Gerência de Configuração - Servidor Rodando!");
+		System.out.println("==> Gerencia de Configuracao - Servidor Rodando!");
 	}
+
 }
