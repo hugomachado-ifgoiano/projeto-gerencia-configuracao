aa0ae0cc8df96a5e1bc52fe9952a3e6e33794476
Hugo Machado Mendes da Silva <hugo.mendes@estudante.ifgoiano.edu.br>
2025-10-31 04:16:07 -0300

    :recycle: refactor: Refatora abertura do Servidor

--- backend/src/main/java/br/com/hugomachadodev/BackendGerenciaDeConfiguracao/BackendGerenciaDeConfiguracaoApplication.java
+++ backend/src/main/java/br/com/hugomachadodev/BackendGerenciaDeConfiguracao/BackendGerenciaDeConfiguracaoApplication.java
@@ -1,17 +1,10 @@
 package br.com.hugomachadodev.BackendGerenciaDeConfiguracao;
-
-import java.util.Scanner;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendGerenciaDeConfiguracaoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendGerenciaDeConfiguracaoApplication.class, args);
-
 		System.out.println("==> Servidor Online!");
 	}
-
 }
